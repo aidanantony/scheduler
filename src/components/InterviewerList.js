1d23517b3fc8bfea6d2bdaa90d591b33c9ab8725
Aidan Antony <aidanantony65@gmail.com>
2022-08-11 14:42:27 -0600

    Initial component setup

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+export default function InterviewerList(props) {
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
+  );
+}
