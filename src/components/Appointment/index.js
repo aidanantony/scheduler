33d92485f4736b56a31fe3701d0a4385c4c9bf26
Aidan Antony <aidanantony65@gmail.com>
2022-09-09 00:46:01 -0600

    Add consts and use of functions passed

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,34 @@ import "components/Appointment/styles.scss";
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
+import useVisualMode from 'hooks/useVisualMode';
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ?
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
-          interviewer={props.interview.interviewer} /> : <Empty />}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+        interviewers = {[]}
+        onCancel = {back}
+        />
+      )}
     </article>
   );
 }
