2afa34408d30cd6a2bb7e3843c304eb9e032311f
Aidan Antony <aidanantony65@gmail.com>
2022-09-22 21:17:51 -0600

    Add cypress testing

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,20 @@
+// navigation.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+  
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
+  });
+});
